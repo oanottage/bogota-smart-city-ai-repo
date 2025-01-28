128fe1009f194f21e90f420f30b22702853e2646
Onassis Nottage <oanottage@gmail.com>
2025-01-28 09:19:56 -0500

    Initial commit with Cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An end to end AI-driven project for improving mobility in Bogota',
+    author='Onassis Nottage',
+    license='MIT',
+)
